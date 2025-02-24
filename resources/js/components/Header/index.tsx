2c019c9e2a02558d2c19da72f74276db008112fa
Jean Guimarães <guimaraes.jean95@gmail.com>
2025-02-24 17:48:59 -0300

    feat: add navigation link to home in Header component

--- resources/js/components/Header/index.tsx
+++ resources/js/components/Header/index.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router"
+
 const Header = () => {
   return (
     <header className="h-[3.125rem] bg-white flex items-center justify-center shadow-header mb-[1.875rem]">
+      <Link to="/">
       <h1 className="font-bold text-green-teal">SWStarter</h1>
+      </Link>
     </header>
   )
 }
